04ec300323d01e52a8c7f77beaea1cc4163cda62
muntaha <muntahamirza890@gmail.com>
2025-02-23 11:12:57 +0500

    Added Delete and Update Functionality

--- routes/blog.js
+++ routes/blog.js
@@ -92,7 +92,7 @@ router.get('/posts/:id/edit', async function (req, res) {
 router.post('/posts/:id/edit', async function (req, res) {
   const enteredTitle = req.body.title;
   const enteredContent = req.body.content;
-  const postId = new ObjectId(req.params.id);
+  //const postId = new ObjectId(req.params.id);
 
   if (
     !enteredTitle ||
@@ -110,22 +110,16 @@ router.post('/posts/:id/edit', async function (req, res) {
     res.redirect(`/posts/${req.params.id}/edit`);
     return; 
   }
-
-  await db
-    .getDb()
-    .collection('posts')
-    .updateOne(
-      { _id: postId },
-      { $set: { title: enteredTitle, content: enteredContent } }
-    );
+  const post =new Post(enteredTitle ,enteredContent,req.params.id);
+  await post.save();
 
   res.redirect('/admin');
 });
 
 router.post('/posts/:id/delete', async function (req, res) {
   const postId = new ObjectId(req.params.id);
-  await db.getDb().collection('posts').deleteOne({ _id: postId });
-
+ const post =new Post(null,null,req.params.id);
+await post.delete();
   res.redirect('/admin');
 });
 
